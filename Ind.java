3c7d38c4977def191f955676101f37d030524f0b
Abishake10 <41162688+Abishake10@users.noreply.github.com>
2018-07-17 22:13:52 +0530

    Create Ind.java

---
+++ Ind.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Ind
+{
+	public static void main(String args[])
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		for(int i=n-1;i>=0;i--)
+		{
+			if(a[i]==i)
+			{
+				System.out.print(a[i]);
+			}
+		}
+	}
+}
